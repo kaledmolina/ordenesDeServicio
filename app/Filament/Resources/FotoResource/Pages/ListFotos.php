fc27afa5b95253cf45f7a0dd810703d19af4ff17
kaledmolina <kaledmoly@gmail.com>
2025-12-30 09:16:22 -0500

    feat: disable create action on Foto list page

--- app/Filament/Resources/FotoResource/Pages/ListFotos.php
+++ app/Filament/Resources/FotoResource/Pages/ListFotos.php
@@ -13,7 +13,7 @@ class ListFotos extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
