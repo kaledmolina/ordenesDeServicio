3b2c1a9e7b2234c74c59e77f147508f3a098b210
kaledmolina <kaledmoly@gmail.com>
2026-01-07 15:24:56 -0500

    chore: remove daily technician ranking widget and update dashboard ranking grid to single column

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,13 +1,12 @@
 <x-filament-panels::page>
-@livewire(\App\Filament\Widgets\OrderStatsOverview::class)
+    @livewire(\App\Filament\Widgets\OrderStatsOverview::class)
 
-<div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-    @livewire(\App\Filament\Widgets\RankingTecnicosDia::class)
-    @livewire(\App\Filament\Widgets\RankingTecnicosMes::class)
-</div>
+    <div class="mt-6 grid grid-cols-1 gap-6">
+        @livewire(\App\Filament\Widgets\RankingTecnicosMes::class)
+    </div>
 
-{{-- Renderiza el widget de la tabla en un nuevo contenedor, forzándolo a ocupar todo el ancho. --}}
-<div class="mt-6">
-    @livewire(\App\Filament\Widgets\FilteredOrdersTable::class, ['status' => $this->status])
-</div>
+    {{-- Renderiza el widget de la tabla en un nuevo contenedor, forzándolo a ocupar todo el ancho. --}}
+    <div class="mt-6">
+        @livewire(\App\Filament\Widgets\FilteredOrdersTable::class, ['status' => $this->status])
+    </div>
 </x-filament-panels::page>
