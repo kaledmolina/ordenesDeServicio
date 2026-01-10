e2d34505ae23578476a852e2bb68e3d37288e1b1
kaledmolina <kaledmoly@gmail.com>
2026-01-10 09:44:29 -0500

    witget eleiminado

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,9 +1,7 @@
 <x-filament-panels::page>
     @livewire(\App\Filament\Widgets\OrderStatsOverview::class)
 
-    <div class="mt-6 grid grid-cols-1 gap-6">
-        @livewire(\App\Filament\Widgets\RankingTecnicosMes::class)
-    </div>
+
 
     {{-- Renderiza el widget de la tabla en un nuevo contenedor, forzándolo a ocupar todo el ancho. --}}
     <div class="mt-6">
